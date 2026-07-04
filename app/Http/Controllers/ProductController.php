eb63b11f4effc2521c6f1a21471182153eb2203c
Jc Calayag <138150245+Jccqt@users.noreply.github.com>
2026-07-04 16:50:21 +0800

    feat(product): add index functionality in ProductController

--- app/Http/Controllers/ProductController.php
+++ app/Http/Controllers/ProductController.php
@@ -3,6 +3,8 @@
 namespace App\Http\Controllers;
 
 use Illuminate\Http\Request;
+use App\Models\Product;
+use App\Http\Resources\ProductResource;
 
 class ProductController extends Controller
 {
@@ -11,7 +13,9 @@ class ProductController extends Controller
      */
     public function index()
     {
-        //
+        $products = Product::all();
+
+        return ProductResource::collection($products);
     }
 
     /**
